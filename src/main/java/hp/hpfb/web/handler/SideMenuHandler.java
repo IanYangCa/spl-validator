b6f31659163b55882b9f15cfec169ec7a37d069a
IanYangCa <IanYang.ca@gmail.com>
2018-07-27 14:13:53 -0400

    Changed the default context path

--- src/main/java/hp/hpfb/web/handler/SideMenuHandler.java
+++ src/main/java/hp/hpfb/web/handler/SideMenuHandler.java
@@ -47,8 +47,8 @@ public class SideMenuHandler {
     private void checkMenuLink(List<SectionMenu> menus) {
 		for(SectionMenu main : menus) {
 			for(MenuLink sub : main.getMenuLinks()) {
-				if(! sub.getHref().startsWith("/IP602")) {
-					sub.setHref("/IP602" + sub.getHref());
+				if(! sub.getHref().startsWith("/spl-validator")) {
+					sub.setHref("/spl-validator" + sub.getHref());
 				} else {
 					return ;
 				}
