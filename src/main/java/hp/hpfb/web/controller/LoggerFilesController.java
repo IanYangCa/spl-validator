5b34cb7b6890d8bd954ba38848473d5020b7a2ff
IanYangCa <IanYang.ca@gmail.com>
2018-07-30 13:57:34 -0400

    Test

--- src/main/java/hp/hpfb/web/controller/LoggerFilesController.java
+++ src/main/java/hp/hpfb/web/controller/LoggerFilesController.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,27 +20,27 @@ import hp.hpfb.web.model.UserFile;
 public class LoggerFilesController {
 
 	@RequestMapping(value="/admin/logfiles", method=RequestMethod.GET)
-    public String renderXml(Model model, HttpServletRequest req) throws Exception {
+    public String renderXml(Model model, HttpServletRequest req){
 		model.addAttribute("userFile", new UserFile());
-      String userPath = "../logs/";
-      model.addAttribute("files", loadAll(userPath));
+      String userPath = "../../logs/";
+      try {
+	    model.addAttribute("files", loadAll(userPath));
 		return "logfiles";
+      } catch(Exception e) {
+			model.addAttribute("errorMsg",  "Errors:\n" + StringUtils.join(e.getStackTrace(), "\n"));
+			return "error";
+      }
     }
     public List<String> loadAll(String root){
     	
-    	try {
-    		File dir = new File(root);
-    		String[] list = dir.list(new FilenameFilter() {
-    			@Override
-    			public boolean accept(File dir, String name) {
-    				return name.endsWith(".log");
-    			}
-    		});
-    		return Arrays.stream(list).map(item -> "/spl-validator/admin/logfile/".concat(item)).collect(Collectors.toList());
-		} catch(Throwable e) {
-			e.printStackTrace();
-		}
-    	return null;
+		File dir = new File(root);
+		String[] list = dir.list(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".log");
+			}
+		});
+		return Arrays.stream(list).map(item -> "/spl-validator/admin/logfile/".concat(item)).collect(Collectors.toList());
     }
 
 }
