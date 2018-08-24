00565fba68cd254013da1c8c96afe654975b1e61
IanYangCa <IanYang.ca@gmail.com>
2018-08-24 11:02:40 -0400

    fixed the oids directory is not right

--- src/main/java/hp/hpfb/web/controller/ValidationXmlController.java
+++ src/main/java/hp/hpfb/web/controller/ValidationXmlController.java
@@ -108,12 +108,11 @@ public class ValidationXmlController {
             	Map<String, String> params = new HashMap<String, String>();
             	
             	params.put("display-language",  p.getDisplayLanguage());
-            	utilities.renderXml(utilities.DEST_RULE_DIR + Utilities.TARGET_BUSINESS_RULE_FILE + Utilities.XSLT, outputDir + "strip.xml", outputDir + "report0.xml", params);
             	params.put("oid_loc", utilities.OIDS_DIR);
             	params.put("id",  file.getOriginalFilename());
-            	params.put("rule-file", utilities.SRC_RULES_DIR + "hc-rules.xml");
             	params.put("property-file", outputDir + Utilities.PROPERTITIES + Utilities.XML);
             	logger.info("oid_loc:" + utilities.OIDS_DIR);
+            	utilities.renderXml(utilities.DEST_RULE_DIR + Utilities.TARGET_BUSINESS_RULE_FILE + Utilities.XSLT, outputDir + "strip.xml", outputDir + "report0.xml", params);
 				utilities.renderXml(utilities.SRC_RULES_DIR + "report.xslt", outputDir + "report0.xml", outputDir + "report.xml", params );
 				Report report = utilities.getReportMsgs(outputDir);
 				if(report.getReportMessage() != null && report.getReportMessage().size() > 0) {
